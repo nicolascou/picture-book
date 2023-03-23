83f05fbeaf871d823b4c4a814c15e74043bd84cf
nicolascou <nicolascousillas1@gmail.com>
2023-03-23 15:07:49 +0100

    setup gh-pages

--- src/features/favorites/favoriteSlice.ts
+++ src/features/favorites/favoriteSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-import { LikedPhoto, Photo } from '../../types';
+import { LikedPhoto } from '../../types';
 
 interface FavoriteState {
   likedPhotos: LikedPhoto[];
