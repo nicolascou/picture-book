1aae99c77b51ebd614a94b98bcdccd029080035f
nicolascou <nicolascousillas1@gmail.com>
2023-03-23 16:24:36 +0100

    fix title link

--- src/components/global/Header.tsx
+++ src/components/global/Header.tsx
@@ -8,7 +8,7 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = ({ inFavorites=false }) => {
   return (
     <header className='header'>
-      <h1 className='header__title'><a href="/">PICTURE BOOK</a></h1>
+      <h1 className='header__title'><a href="/picture-book/">PICTURE BOOK</a></h1>
       <div className='header__btn'>
         <Button href={inFavorites ? '/picture-book/search/' : '/picture-book/my-photos/'} 
         className='secondary-btn'>{inFavorites ? 'GO BACK TO SEARCH 🔎':  'MY PHOTOS 📷'}</Button>
