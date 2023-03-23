7d7503cd73d91f28c0bdb5a0e767c76c6e907406
nicolascou <nicolascousillas1@gmail.com>
2023-03-23 12:48:56 +0100

    add removePhoto from likedPhotos

--- src/components/favs/LikedPhotoIcons.tsx
+++ src/components/favs/LikedPhotoIcons.tsx
@@ -1,15 +1,25 @@
 import React from 'react'
+import { useAppSelector, useAppDispatch } from '../../app/hooks';
+import { loadLikedPhotos } from '../../features/favorites/favoriteSlice';
 
 interface LikedPhotoIconsProps {
   photoId: string;
 }
 
 const LikedPhotoIcons: React.FC<LikedPhotoIconsProps> = ({ photoId }) => {
-  
+  // const { likedPhotos } = useAppSelector(state => state.favoriteReducer);
+  const dispatch = useAppDispatch()
+
+  const removePhoto = () => {
+    let array: any[] = JSON.parse(localStorage.getItem('picture-book') || '[]');
+    array = array.filter((photo) => photo.id !== photoId);
+    localStorage.setItem('picture-book', JSON.stringify(array));
+    dispatch( loadLikedPhotos() )
+  }
   
   return (
     <>
-      <button className='favorites__photos__trash'>
+      <button className='favorites__photos__trash' onClick={removePhoto}>
         <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill='white'>
           <path d="M19 24h-14c-1.104 0-2-.896-2-2v-16h18v16c0 1.104-.896 2-2 2m3-19h-20v-2h6v-1.5c0-.827.673-1.5 1.5-1.5h5c.825 0 1.5.671 1.5 1.5v1.5h6v2zm-12-2h4v-1h-4v1z"/>
         </svg>
